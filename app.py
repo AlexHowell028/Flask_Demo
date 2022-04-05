ff43f13644a421a7595fef19e06f7fe1183b801f
Alex Howell <ahwell@uiowa.edu>
2022-04-04 21:55:45 -0500

    update at 9:55

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect, request, url_for
 
 app = Flask(__name__)
 
+friend_list = [{"name": "Alex Howell"} ]
+
 @app.route('/')
 def index():
-    return render_template('index.html', pageTitle='Flask Server Home Page')
+    return render_template('index.html', pageTitle='Alex\'s Friends', friends = friend_list)
 
-@app.route('/mike') #don't really need
+@app.route('/alex') 
 def mike():
-    return render_template('mike.html', pageTitle= 'About Mike')
+    return render_template('alex.html', pageTitle= 'About Alex')
 
-@app.route('/amy') #don't really need
-def amy():
-    return render_template('amy.html', pageTitle= 'About Amy')
+@app.route('/add_friend', methods=['GET', 'POST']) 
+def add_friend():
+    if request.method == 'POST':
+        form = request.form
+        fname = form['fname']
+        lname = form['lname']
+        friend_dict = {"name": fname + " " + lname}
+        friend_list.append(friend_dict)
+        return redirect(url_for('index'))
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
