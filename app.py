c0a23b6a26162a924b34d719e6e8b0e4b42b5a8b
Alex Howell <ahwell@uiowa.edu>
2022-04-04 19:58:12 -0500

    project updates

--- app.py
+++ app.py
@@ -5,11 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', pageTitle='Flask Server Home Page')
 
-@app.route('/mike')
+@app.route('/mike') #don't really need
 def mike():
-    return render_template('mike.html')
+    return render_template('mike.html', pageTitle= 'About Mike')
+
+@app.route('/amy') #don't really need
+def amy():
+    return render_template('amy.html', pageTitle= 'About Amy')
 
 if __name__ == '__main__':
     app.run(debug=True)
