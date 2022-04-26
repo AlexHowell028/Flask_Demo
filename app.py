b4ee383b55826446f9d850e2b0c4c3b139025e0a
Alex Howell <78935567+AlexHowell028@users.noreply.github.com>
2022-04-25 22:14:50 -0500

    update 4.25 10:14p

--- app.py
+++ app.py
@@ -1,39 +1,19 @@
 from flask import Flask
-from flask import render_template, redirect, request, url_for
+from flask import render_template
 
 app = Flask(__name__)
 
-friend_list = [{"name": "Alex Howell", "email":"alex@howell.com"} ]
-
 @app.route('/')
 def home():
-    return render_template('home.html', pageTitle='Vertical Tank Maintenance', friends = friend_list)
+    return render_template('home.html')
 
 @app.route('/estimate') 
-def index():
-    return render_template('index.html', pageTitle= 'Estimate')
-
-
-#@app.route('/home') 
-#def friend():
- #   return render_template('home.html', pageTitle= 'Home')
-
+def estimate():
+    return render_template('index.html')
 
 @app.route('/about') 
-def mike():
-    return render_template('about.html', pageTitle= 'About VTM')
-
-@app.route('/add_friend', methods=['GET', 'POST']) 
-def add_friend():
-    if request.method == 'POST':
-        form = request.form
-        fname = form['fname']
-        lname = form['lname']
-        email = form['email']
-        friend_dict = {"name": fname + " " + lname, "email": email}
-        friend_list.append(friend_dict)
-        return redirect(url_for('index'))
-    return redirect(url_for('index'))
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
