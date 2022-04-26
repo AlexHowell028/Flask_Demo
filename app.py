c1ef0f617ee8cf7af9f7dc76448b2244220e59a8
Alex Howell <78935567+AlexHowell028@users.noreply.github.com>
2022-04-26 14:00:27 -0500

    4/26 update 2pm

--- app.py
+++ app.py
@@ -15,11 +15,33 @@ def estimate():
 def about():
     return render_template('about.html')
 
+#(Process)Modify app.py to collect the data from your estimate.html form into variables. Calculate the cost estimate
+#based on the formulas found in the details section above
+#(Output)The calculation should produce a value stored in a varaible that is displayed on estimate.html after processing
+
 @app.route('/index', methods=['GET','POST'])
 def calculate_tank_top(tank_top):
     tank_top= 3.14*['tankRadius']
     return tank_top
     
+def calculate_tank_sides(tank_sides):
+    tank_sides=2(3.14(['tankRadius']*['tankHeight']))
+
+def calculate_total_area_in(total_area_in):
+    tank_area_in= tank_top + tank_sides
+
+def calculate_total_sqft(total_sqft):
+    total_sqft=total_area_in/144
+
+def calculate_material_cost(material_cost):
+    material_cost= total_sqft*25
+
+def calculate_labor_cost(labor_cost):
+    labor_cost=toal_sqft*15
+
+def calculate_total_cost(total_cost):
+    total_cost= material_cost + labor_cost
+    print (total_cost)
 
 
 
